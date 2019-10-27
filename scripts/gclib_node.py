a7b4e98df87ca8b5d77dfcf4dd24fda78edde011
Tomas Baca <tomas.baca@fel.cvut.cz>
2019-10-27 17:39:18 +0100

    inital commit

---
+++ scripts/gclib_node.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+
+import rospy
+
+from gclib_python_wrapper.gclib_wrapper import GclibWrapper
+
+from gclib_ros.srv import SingleAxisMotion,SingleAxisMotionResponse
+from gclib_ros.srv import MultiAxisMotion,MultiAxisMotionResponse
+from gclib_ros.msg import Position
+
+class Gclib:
+
+    def mainTimer(self, event):
+
+        rospy.loginfo_once('Main timer spinning')
+
+        msg = Position()
+
+        msg.stamp = rospy.Time.now()
+
+        positions = []
+
+        for i in range(0, self.n_stages):
+
+            position = self.wrapper.getPositionUnit(i)
+            positions.append(position)
+
+            msg.position.append(position)
+
+        self.publisher_position.publish(msg)
+
+        rospy.loginfo_throttle(1.0, 'Position {}'.format(positions))
+
+    def callbackGotoRelative(self, req):
+
+        rospy.loginfo('Goto relative axis {} value {}'.format(req.axis, req.value))
+
+        res = self.wrapper.moveRelative(req.axis, req.value)
+
+        if res:
+          response = SingleAxisMotionResponse(True, "we are there")
+          rospy.loginfo('Goto absolute finished')
+        else:
+          response = SingleAxisMotionResponse(True, "something went wrong")
+          rospy.loginfo('Goto absolute finished')
+
+        return response
+
+    def callbackGotoSingle(self, req):
+
+        rospy.loginfo('Goto axis {} value {}'.format(req.axis, req.value))
+
+        res = self.wrapper.moveAbsolute(req.axis, req.value)
+
+        if res:
+          response = SingleAxisMotionResponse(True, "we are there")
+          rospy.loginfo('Goto absolute finished')
+        else:
+          response = SingleAxisMotionResponse(True, "something went wrong")
+          rospy.logerr('Goto absolute failed')
+
+        return response
+
+    def callbackGotoAll(self, req):
+
+        rospy.loginfo('Goto values [{}]'.format(req.values))
+
+        res = self.wrapper.moveAllAbsolute(req.values)
+
+        if res:
+          response = MultiAxisMotionResponse(True, "we are there")
+          rospy.loginfo('Goto all finished')
+        else:
+          response = MultiAxisMotionResponse(True, "something went wrong")
+          rospy.logerr('Goto all failed')
+
+        return response
+
+    def __init__(self):
+
+        rospy.init_node('gclib', anonymous=True)
+
+        # parameters
+        address = rospy.get_param('~address')
+        publisher_rate = rospy.get_param('~publisher_rate')
+        dummy = rospy.get_param('~dummy')
+
+        self.n_stages = rospy.get_param('~n_stages')
+
+        steps2unit = rospy.get_param('~steps2unit')
+
+        speed = rospy.get_param('~speed')
+        acceleration = rospy.get_param('~acceleration')
+        deceleration = rospy.get_param('~deceleration')
+
+        forward_limit = rospy.get_param('~forward_limit')
+        backward_limit = rospy.get_param('~backward_limit')
+
+        self.wrapper = GclibWrapper(2, steps2unit, dummy)
+
+        self.wrapper.open(address)
+
+        self.wrapper.setOrigin()
+
+        self.wrapper.setSpeed(speed)
+        self.wrapper.setAcceleration(acceleration)
+        self.wrapper.setDeceleration(deceleration)
+
+        self.wrapper.setForwardLimit(forward_limit)
+        self.wrapper.setBackwardLimit(backward_limit)
+
+        self.wrapper.motorsOn()
+
+        rospy.Timer(rospy.Duration(1.0/publisher_rate), self.mainTimer)
+
+        rospy.Service("~goto_relative", SingleAxisMotion, self.callbackGotoRelative)
+        rospy.Service("~goto_absolute", SingleAxisMotion, self.callbackGotoSingle)
+        rospy.Service("~goto", MultiAxisMotion, self.callbackGotoAll)
+
+        self.publisher_position = rospy.Publisher("~position", Position, queue_size=1)
+
+        rospy.spin()
+
+if __name__ == '__main__':
+    try:
+        gclib = Gclib()
+    except rospy.ROSInterruptException:
+        pass
